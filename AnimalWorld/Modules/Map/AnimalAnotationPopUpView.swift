427b1242194590e59574676a69b1d566d9873b4d
zhany <zhanybeksalgarin@gmail.com>
2023-12-15 15:14:57 -0800

    Refactor

--- AnimalWorld/Modules/Map/AnimalAnotationPopUpView.swift
+++ AnimalWorld/Modules/Map/AnimalAnotationPopUpView.swift
@@ -12,12 +12,21 @@ struct AnimalAnotationPopUpView: View {
     @Binding var isAnimalAnitationShown: Bool
     
     var body: some View {
-        ZStack {
-            Color(.white)
+        Color(.white)
+            .opacity(0.3)
+        VStack {
             Text("Some long ass text about animal")
+                .bold()
+                .padding(20)
             Button("😺Okay😺") {
                 isAnimalAnitationShown.toggle()
             }
+            .bold()
+            .padding(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.brown, lineWidth: 5)
+            )
         }
     }
 }
